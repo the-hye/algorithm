d9c35af345369a6f6d885b946409fc344eaf1ca1
the-hye <ganzi410@gmail.com>
2021-05-18 14:10:03 +0900

    algorithm with hw

---
+++ Baekjoon11651.py
@@ -0,0 +1,70 @@
+import time
+
+# x 와 y 위치 switch
+def change(x):
+    for i in range(N):
+        result[i][0], result[i][1] = result[i][1], result[i][0]
+
+# 최대힙 생성
+def heap(list):
+    for i in range(len(list)):
+        c = i
+        while c > 0:
+            root = (c-1)//2
+            if list[c][0] > list[root][0]:
+                list[c],list[root] = list[root],list[c]
+            elif list[c][0] == list[root][0]:
+                if list[c][1] > list[root][1]:
+                    list[c], list[root] = list[root], list[c]
+            c = root
+
+
+# 힙 정렬
+def heapsort(list):
+    heap(list)
+    for i in range(len(list)-1, -1, -1):
+        list[0], list[i] = list[i], list[0]
+        root = 0
+        c = 0
+        while(c < i):
+            c = (2 * root) + 1
+            if c < i-1 and list[c][0] < list[c+1][0]:
+               c += 1
+            elif c < i-1 and list[c][0] == list[c+1][0]:
+                if list[c][1] < list[c+1][1]:
+                    c += 1
+            if c < i and list[root][0] < list[c][0]:
+                list[root], list[c] = list[c], list[root]
+            elif c < i and list[root][0] == list[c][0]:
+                if list[root][1] < list[c][1]:
+                    list[root], list[c] = list[c], list[root]
+            root = c
+
+# read file
+f = open('/Users/jeje/Desktop/Homework2_1713201_석지혜/homework2_input_file_1.txt')
+
+# 총 좌표의 개수
+N = int(f.readline())
+
+# 결과 저장 리스트
+result = []
+
+# make list
+for i in range(N):
+    result.append(f.readline().split())
+
+start = time.time()
+# x,y 좌표 변경
+change(result)
+# 정렬
+heapsort(result)
+# x 와 y 위치 switch
+change(result)
+# 수행 시간
+print("수행시간 : ", time.time()-start)
+
+# 결과출력
+for i in range(N):
+    print(i+1,":",result[i][0],result[i][1])
+
+
---
+++ bubblesort.py
@@ -0,0 +1,47 @@
+import time
+
+# 내림차순 정렬 함수
+def sortRank(x):
+    for i in range(len(x)-1):
+        for j in range(i+1, len(x)):
+            if x[i] < x[j]:
+                x[i], x[j] = x[j], x[i]
+
+# read file
+f = open('/Users/jeje/Desktop/Homework2_1713201_석지혜/homework2_input_file_2.txt')
+
+# R, N, S 추출 및 데이터 정리
+data = f.readline().split(', ')  # , 기준 split
+data = [word.strip(')\n').strip('(') for word in data]  # 괄호와 엔터 제거
+R = int(data[0])  # 10000
+N = int(data[1])  # 5000
+S = []
+for i in range(2, len(data)):
+    S.append(data[i])
+S = list(map(int, S))
+
+# 기존 랭킹 R개 입력
+ranking = []
+for i in range(R):
+    score = f.readline().split(' ')[1]  # 띄어쓰기 기준 split
+    ranking.append(int(score))
+
+# ranking 내림차순
+sortRank(ranking)
+# N개로 ranking 수정
+ranking = ranking[:N]
+
+start = time.time()
+# S 순위 추출
+for i in S:
+    # S[i]이 ranking의 마지막 값보다 작을 때 -> 순위권 밖
+    if i < ranking[-1]:
+        print("Score", i, ": -1")
+    # 순위권 안 : ranking에 입력 후 재정렬
+    else:
+        ranking.append(i)
+        sortRank(ranking)
+        print("Score", i, ":", ranking.index(i)+1)
+
+# 수행시간
+print("수행시간 : ", time.time()-start)
